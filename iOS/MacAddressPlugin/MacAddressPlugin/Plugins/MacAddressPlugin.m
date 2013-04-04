56e3aceb030c1f8d4bcf13d7024a815cdcc47c95
Julio Cesar <jcesarmobile@gmail.com>
2013-04-04 21:14:02 +0200

    added iOS MacAddressPlugin
    iOS Mac Address plugin with sample project

---
+++ iOS/MacAddressPlugin/MacAddressPlugin/Plugins/MacAddressPlugin.m
@@ -0,0 +1,55 @@
+//
+//  MacAddressPlugin.m
+//  MacAddressPlugin
+//
+//  Created by Admin on 04/04/13.
+//
+//
+
+#import "MacAddressPlugin.h"
+#import "IPAddress.h"
+
+@implementation MacAddressPlugin
+
+- (void)getMacAddress:(CDVInvokedUrlCommand*)command {
+    
+    CDVPluginResult* pluginResult = nil;
+    InitAddresses();
+    GetIPAddresses();
+    GetHWAddresses();
+
+    NSMutableArray *resultArray = [[NSMutableArray alloc]init];
+    for (int i=0; i<MAXADDRS; ++i)
+    {
+
+        unsigned long theAddr;
+        
+        theAddr = ip_addrs[i];
+        
+        if (theAddr == 0) break;
+
+        
+        NSMutableDictionary * dict = [[NSMutableDictionary alloc]init];
+        [dict setValue:[NSString stringWithCString:if_names[i] encoding:NSUTF8StringEncoding]  forKey:@"name"];
+        [dict setValue:[NSString stringWithCString:hw_addrs[i] encoding:NSUTF8StringEncoding]  forKey:@"macAddress"];
+        [dict setValue:[NSString stringWithCString:ip_names[i] encoding:NSUTF8StringEncoding]  forKey:@"ip"];
+        [resultArray addObject:dict];
+
+    }
+    
+    if ([resultArray count]>0) {
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:resultArray];
+        
+    } else {
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        
+    }
+    
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    
+}
+
+@end
